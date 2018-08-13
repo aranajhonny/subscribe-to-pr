7a0f1b31ea915dd9bf391484fee93910e8014011
Me <me@example.com>
2018-08-13 20:46:05 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { root, repository } = program.refs
 
 export async function init() {
+  await repository.pullRequestOpened.subscribe('onPull');
+}
 
+export async function onPull({ args }) {
+  typeof args;
 }
