b6f47611da40a6ac5fa9a062960b6dc63cc594b7
Me <me@example.com>
2018-08-13 21:50:02 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+import axios from 'axios';
+
 const { root, repository } = program.refs
 
 export async function init() {
   await repository.pullRequestOpened.subscribe('onPull');
 }
 
-export async function onPull({ args }) {
+export async function onPull({ args }) {  
   const { diffUrl } = await args.pullRequest.$query({ diffUrl });
-  
-  // fetch git diff
-  // TODO: axios call
-  // if diff is true
-  // const isFile = diff.includes(path);
-  //if(isFile){
-  // await args.issue.subscribe();
-  //}
+/* 
+  const client = axios.create({
+    headers: {'Content-Type': 'Accept: application/vnd.github.diff'}
+  });
+
+  const diff = await client.get(diffUrl);
+ 
+  const hasFile = diff.includes(path);
+ 
+  if(hasFile){
+    await args.issue.subscribe();
+  } */
 }
